edf9a33085721e358929db83554dbf5021a66c61
Cordaz <andrea1.corneo@mail.polimi.it>
2017-03-09 12:47:56 +0100

    Implemented pseudo count

--- OligosCounter/counter.py
+++ OligosCounter/counter.py
@@ -18,7 +18,7 @@ def get_reads(f):
       if i != 2:
          continue
       reads.append(drop_newline(line))
-   
+
    return reads
 
 
@@ -26,7 +26,7 @@ def get_kmers(read):
    kmers = []
    for i in range(0, len(read)-k+1):
       kmers.append(read[i:i+k])
-   
+
    return kmers
 
 
@@ -39,7 +39,7 @@ def complementary_kmer(kmer):
    c_kmer = ""
    for char in reversed(kmer):
       c_kmer = c_kmer + comp_bases[char]
-   
+
    return c_kmer
 
 def is_palyndrome(kmer, c_kmer):
@@ -49,7 +49,7 @@ def is_palyndrome(kmer, c_kmer):
          palyndrome = False
       if not palyndrome:
          break
-   
+
    return palyndrome
 
 
@@ -66,29 +66,32 @@ del args[0]
 kmers_hash = {}
 kmer_total = 0
 
-input_file = open(input_file_path, "r")
-reads = get_reads(input_file)
+with open(input_file_path, "r") as input_file:
+    reads = get_reads(input_file)
 
-for read in reads:
-   kmers = get_kmers(read)
-   
-   for kmer in kmers:
-      if is_valid_kmer(kmer):
-         if kmer in kmers_hash:
-            kmers_hash[kmer] = kmers_hash[kmer] + 1
-         else:
-            kmers_hash[kmer] = 1
-         kmer_total = kmer_total + 1
-         c_kmer = complementary_kmer(kmer)
-         if not is_palyndrome(kmer, c_kmer):
-            if c_kmer in kmers_hash:
-               kmers_hash[c_kmer] = kmers_hash[c_kmer] + 1
-            else:
-               kmers_hash[c_kmer] = 1
-            kmer_total = kmer_total + 1
+    for read in reads:
+       kmers = get_kmers(read)
+
+       for kmer in kmers:
+          if is_valid_kmer(kmer):
+             if kmer in kmers_hash:
+                kmers_hash[kmer] = kmers_hash[kmer] + 1
+             else:
+                kmers_hash[kmer] = 2
+                kmer_total = kmer_total + 1
+             kmer_total = kmer_total + 1
+             c_kmer = complementary_kmer(kmer)
+             if not is_palyndrome(kmer, c_kmer):
+                if c_kmer in kmers_hash:
+                   kmers_hash[c_kmer] = kmers_hash[c_kmer] + 1
+                else:
+                   kmers_hash[c_kmer] = 2
+                   kmer_total = kmer_total + 1
+                kmer_total = kmer_total + 1
 
 output_file = open(output_file_path, "w")
 output_file.write("k-mer\tcount\tfreq\n")
+output_file.write("@total\t" + str(kmer_total) + "\n")
 for kmer in sorted(kmers_hash):
    output_file.write(str(kmer) + "\t" + str(kmers_hash[kmer]) + "\t" + str(float(kmers_hash[kmer]) / kmer_total) + "\n")
-   
+output_file.close()
--- OligosCounter/over_representation_check.py
+++ OligosCounter/over_representation_check.py
@@ -4,6 +4,13 @@ import sys
 import matplotlib.pyplot as plt
 import math
 
+def get_total(string):
+    args = string.strip().split("\t")
+    if args[0] == "@total":
+        return int(args[1])
+    return -1
+
+
 args = sys.argv[1:]
 if len(args) < 3:
    print "usage: [--plot] input_file control_file output_file"
@@ -23,34 +30,42 @@ output_file_path = args[0]
 del args[0]
 
 
-control_file = open(control_file_path, "r")
-next(control_file) #skip header
-#Create lookup table
-lookup = {}
-for line in control_file:
-   (kmer, count, freq) = line.strip().split("\t")
-   lookup[kmer] = (count, freq)
+with open(control_file_path, "r") as control_file:
+    dummy = control_file.readline() #skip header
+    ctrl_tot = get_total(control_file.readline())
+    if ctrl_tot == -1:
+        print "Error: @total not found"
+        sys.exit(1)
+    #Create lookup table
+    lookup = {}
+    for line in control_file:
+       (kmer, count, freq) = line.strip().split("\t")
+       lookup[kmer] = (count, freq)
 
 differential_kmer = {}
 differential_kmer_log = {}
 kmer_total = 0
 diff_total = 0.0
 
-input_file = open(input_file_path, "r")
-next(input_file) #skip header
-for line in input_file:
-   (kmer, count, freq) = line.strip().split("\t")
-   if not lookup.has_key(kmer): #skip if not present
-      continue
+with open(input_file_path, "r") as input_file:
+    dummy = input_file.readline() #skip header
+    input_tot = get_total(input_file.readline())
+    if input_tot == -1:
+        print "Error: @total not found"
+        sys.exit(1)
+    for line in input_file:
+       (kmer, count, freq) = line.strip().split("\t")
+       if lookup.has_key(kmer):
+          (ctrl_count, ctrl_freq) = lookup[kmer]
+       else:
+           (ctrl_count, ctrl_freq) = (1, float(1) / ctrl_tot) #calculate pseudo frequence
+       diff = float(freq) / float(ctrl_freq)
 
-   (ctrl_count, ctrl_freq) = lookup[kmer]
-   diff = float(freq) / float(ctrl_freq)
+       kmer_total = kmer_total + 1
+       diff_total = diff_total + math.log(diff,2)
 
-   kmer_total = kmer_total + 1
-   diff_total = diff_total + math.log(diff,2)
-
-   differential_kmer[kmer] = (count, freq, ctrl_count, ctrl_freq, diff)
-   differential_kmer_log[kmer] = math.log(diff, 2)
+       differential_kmer[kmer] = (count, freq, ctrl_count, ctrl_freq, diff)
+       differential_kmer_log[kmer] = math.log(diff, 2)
 
 diff_mean = diff_total / kmer_total
 
@@ -67,7 +82,8 @@ if plot:
 
 output_file = open(output_file_path + ".txt", "w")
 
-output_file.write("k-mer\tcount\tfreq\tcontrol count\tcontrol freq\tdifference\tdifference log2\n")
+output_file.write("k-mer\tcount\tfreq\tcontrol_count\tcontrol_freq\tdifference\tdifference_log2\n")
 for kmer, log_diff  in sorted(differential_kmer_log.items(), key=lambda x: x[1], reverse = True):
    (count, freq, ctrl_count, ctrl_freq, diff) = differential_kmer[kmer]
    output_file.write(str(kmer) + "\t" + str(count) + "\t" + str(freq) + "\t" + str(ctrl_count) + "\t" + str(ctrl_freq) + "\t" + str(diff) + "\t" + str(log_diff) + "\n")
+output_file.close()
