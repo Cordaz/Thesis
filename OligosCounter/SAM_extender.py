26b6446ed26033c86a690df28198434da9e319b4
Cordaz <andrea1.corneo@mail.polimi.it>
2017-03-09 18:34:29 +0100

    Fixed genome indexing. Fixed global vars. Minor fixes

--- OligosCounter/SAM_extender.py
+++ OligosCounter/SAM_extender.py
@@ -17,7 +17,7 @@ read_length = 34
 #Global vars
 chrom_in_use = ""
 chrom_seq = ""
-chrom_index = 0
+start = 0
 chrom_length = {}
 
 #Functions
@@ -44,22 +44,29 @@ def get_sam_info(line):
     return (args[0], int(args[1]), args[2], int(args[3])) #(>id, strand, chrom, pos)
 
 def get_chromosome_seq(chrom):
+    seq = ""
     with open(genome_path + chrom + ".fa", "r") as chrom_file:
         next(chrom_file)
         for line in chrom_file:
-            global chrom_seq
-            chrom_seq = chrom_seq + line.strip()
-    chrom_index = 0
-
-def check_chrom(chrom):
-    if chrom_in_use != chrom:
-        chrom_in_use = chrom
-        get_chromosome_seq(chrom)
+            seq = seq + line.strip().upper()
+    return seq
 
 
-def extend_read(strand, chrom, pos):
+def check_seq(s):
+    return 'N' not in s
+
+
+def complementary(seq):
+   comp_bases = {'A':'T', 'C':'G', 'G':'C', 'T':'A'}
+   c_seq = ""
+   for char in reversed(seq):
+      c_seq = c_seq + comp_bases[char]
+
+   return c_seq
+
+def extend_read(chrom_index, seq, strand, chrom, pos):
     if strand == not_mapped:
-        return ""
+        return "", chrom_index
 
     while chrom_index < chrom_length[chrom]:
         if chrom_index == pos:
@@ -67,19 +74,28 @@ def extend_read(strand, chrom, pos):
         chrom_index = chrom_index + 1
 
     if strand == pos_strand:
-        return extend_pos()
+        return extend_pos(seq, chrom_index), chrom_index
     if strand == neg_strand:
-        return extend_neg()
+        return extend_neg(seq, chrom_index), chrom_index
+    return "", chrom_index
+
+def extend_pos(chrom_seq, chrom_index):
+    seq = chrom_seq[chrom_index -1 : chrom_index + ext_length - 1]
+    if check_seq(seq):
+        return seq
     return ""
 
-def extend_pos():
-    return chrom_seq[chrom_index : chrom_index + ext_length]
+def extend_neg(chrom_seq, chrom_index):
+    seq = chrom_seq[chrom_index - (ext_length - read_length) - 1 : chrom_index + read_length - 1 - 1]
+    if check_seq(seq):
+        return complementary(seq)
+    return ""
 
-def extend_neg():
-    temp = chrom_seq[chrom_index - (ext_length - read_length) : chrom_index + read_length - 1 ]
-    return temp[::-1]
-
-
+def file_len(fname):
+    with open(fname, "r") as f:
+        for i, l in enumerate(f):
+            pass
+    return i+1
 
 
 #Main
@@ -90,10 +106,11 @@ if len(args) < 1:
     print "usage: input_file"
     sys.exit(1)
 
-input_file_path = args[0]
+input_file_path = dataset_path + args[0]
 
+input_length = file_len(input_file_path)
 
-with open(dataset_path + input_file_path, "r") as input_file:
+with open(input_file_path, "r") as input_file:
     output_file = open(result_path + to_fasta(input_file_path), "w+")
     line = input_file.readline()
     while line[0] == '@':
@@ -103,16 +120,27 @@ with open(dataset_path + input_file_path, "r") as input_file:
     #Process first line before loop
     (id_seq, strand, chrom, pos) = get_sam_info(line)
     chrom_in_use = chrom
-    get_chromosome_seq(chrom)
-    seq = extend_read(strand, chrom, pos)
+    chrom_seq = get_chromosome_seq(chrom)
+    #Seaarch for read from starting point
+    seq, start = extend_read(start, chrom_seq, strand, chrom, pos)
+    progress = 1
     if seq:
         output_file.write(id_seq + "\n" + seq)
 
     #Process rest of file
     for line in input_file:
         (id_seq, strand, chrom, pos) = get_sam_info(line)
-        check_chrom(chrom)
-        seq = extend_read(strand, chrom, pos)
+        if chrom_in_use != chrom:
+            chrom_seq = get_chromosome_seq(chrom)
+            chrom_in_use = chrom
+            start = 0
+
+        seq, start = extend_read(start, chrom_seq, strand, chrom, pos)
         if seq:
             output_file.write("\n" + id_seq + "\n" + seq)
+
+        progress = progress + 1
+        if (float(progress) / input_length) * 100 % 5 == 0:
+            print "*"
+
     output_file.close()
