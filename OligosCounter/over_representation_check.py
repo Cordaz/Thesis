56cd72015958d3b3f80fdac92c1c0d5bb6cfcbd9
Cordaz <andrea1.corneo@mail.polimi.it>
2017-03-09 12:00:05 +0100

    Implemented log2 for graph

--- OligosCounter/over_representation_check.py
+++ OligosCounter/over_representation_check.py
@@ -2,9 +2,7 @@
 
 import sys
 import matplotlib.pyplot as plt
-import numpy as np
-
-threshold = 0.5
+import math
 
 args = sys.argv[1:]
 if len(args) < 3:
@@ -33,8 +31,8 @@ for line in control_file:
    (kmer, count, freq) = line.strip().split("\t")
    lookup[kmer] = (count, freq)
 
-over_represented = {}
 differential_kmer = {}
+differential_kmer_log = {}
 kmer_total = 0
 diff_total = 0.0
 
@@ -44,33 +42,32 @@ for line in input_file:
    (kmer, count, freq) = line.strip().split("\t")
    if not lookup.has_key(kmer): #skip if not present
       continue
-   
+
    (ctrl_count, ctrl_freq) = lookup[kmer]
-   diff = (float(freq) - float(ctrl_freq)) / float(ctrl_freq)
-   
+   diff = float(freq) / float(ctrl_freq)
+
    kmer_total = kmer_total + 1
-   
-   diff_total = diff_total + diff
-   differential_kmer[kmer] = diff
-   
-   if abs(diff) >= threshold:
-      over_represented[kmer] = (count, freq, ctrl_count, ctrl_freq)
+   diff_total = diff_total + math.log(diff,2)
+
+   differential_kmer[kmer] = (count, freq, ctrl_count, ctrl_freq, diff)
+   differential_kmer_log[kmer] = math.log(diff, 2)
 
 diff_mean = diff_total / kmer_total
 
 if plot:
    fig, ax = plt.subplots()
-   ax.bar(range(len(differential_kmer)), differential_kmer.values(), align='center', color='r', edgecolor='r')
+   ax.bar(range(len(differential_kmer_log)), differential_kmer_log.values(), align='center', color='#cc3300', edgecolor='#cc3300')
    #Mean
-   ax.plot((0,len(differential_kmer)), (diff_mean,diff_mean), color='b')
+   ax.plot((0,len(differential_kmer_log)), (diff_mean,diff_mean), color='#3366ff')
+   ax.plot((0,len(differential_kmer_log)), (0,0), color='white')
    ax.axes.get_xaxis().set_visible(False)
 
    fig.savefig(output_file_path + ".pdf", format='pdf')
 
+
 output_file = open(output_file_path + ".txt", "w")
 
-output_file.write("k-mer\tcount\tfreq\tcontrol count\tcontrol freq\tdifference\n")
-for kmer in sorted(over_represented):
-   (count, freq, ctrl_count, ctrl_freq) = over_represented[kmer]
-   output_file.write(str(kmer) + "\t" + str(count) + "\t" + str(freq) + "\t" + str(ctrl_count) + "\t" + str(ctrl_freq) + "\t" + str(differential_kmer[kmer]) + "\n")
-
+output_file.write("k-mer\tcount\tfreq\tcontrol count\tcontrol freq\tdifference\tdifference log2\n")
+for kmer, log_diff  in sorted(differential_kmer_log.items(), key=lambda x: x[1], reverse = True):
+   (count, freq, ctrl_count, ctrl_freq, diff) = differential_kmer[kmer]
+   output_file.write(str(kmer) + "\t" + str(count) + "\t" + str(freq) + "\t" + str(ctrl_count) + "\t" + str(ctrl_freq) + "\t" + str(diff) + "\t" + str(log_diff) + "\n")
