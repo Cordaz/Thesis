c2d0dc6f6931973e7eb5bf113953079ed248167a
Cordaz <andrea1.corneo@mail.polimi.it>
2017-03-24 12:33:42 +0100

    DBG loader lib. <to test>

---
+++ dbg/loader/loader.c
@@ -0,0 +1,108 @@
+graph_t * alloc_graph(double nodes, double edges) {
+	graph_t * dbg;
+	if ( !(dbg->nodes = (node_t**)malloc(sizeof(node_t*) * nodes)) ) {
+		fprintf(stdout, "ERROR: couldn't allocate memory\n");
+		return NULL;
+	}
+	if ( !(dbg->edges = (edge_t**)malloc(sizeof(edge_t*) * edges)) ) {
+		fprintf(stdout, "ERROR: couldn't allocate memory\n");
+		return NULL;
+	}
+	for (i=0; i<nodes; i++) {
+		dbg->nodes[i] = NULL;
+		dbg->edges[i] = NULL;
+	}
+	for( ; i<edges; i++) {
+		dbg->edges[i] = NULL;
+	}
+}
+
+
+graph_t * load_nodes(graph_t * dbg, char * path) {
+	FILE * fp;
+	char buf[64]; //Reading buffer
+	char args[32]; //Args buffer
+	node_t * n;
+
+	if ( !(fp = fopen(path, "r")) ) {
+		fprintf(stdout, "ERROR: can't open %s\n", path);
+		return NULL;
+	}
+
+	fgets(buf, 64, fp);
+	//Header read, skip
+	fgets(buf, 64, fp);
+	//Actually read the first line
+	int l; //Store length
+	while(!feof(fp)) {
+		l =  = strlen(buf);
+		buf[l-1] = '\0' //remove \n
+		if( !(n = (node_t *)malloc(sizeof(node_t))) ) {
+			fprintf(stdout, "ERROR: couldn't allocate memory\n");
+			return NULL;
+		}
+		//Assume the string is correctly formatted
+		if(args = strtok(buf, "\t"))
+			n->id = atoi(args);
+		if(args = strtok(NULL, "\t"))
+			n->seq = args;
+
+		dbg->nodes[n->id] = n;
+
+		fgets(buf, 64, fp);
+	}
+
+	return dbg;
+}
+
+
+graph_t * load_edges(graph_t * dbg, char * path) {
+	FILE * fp;
+	char buf[64]; //Reading buffer
+	char args[32]; //Args buffer
+	edge_t * e;
+
+	if ( !(fp = fopen(path, "r")) ) {
+		fprintf(stdout, "ERROR: can't open %s\n", path);
+		return NULL;
+	}
+
+	fgets(buf, 64, fp);
+	//Header read, skip
+	fgets(buf, 64, fp);
+	//Actually read the first line
+	int l; //Store length
+	while(!feof(fp)) {
+		l =  = strlen(buf);
+		buf[l-1] = '\0' //remove \n
+		if( !(e = (edge_t *)malloc(sizeof(edge_t))) ) {
+			fprintf(stdout, "ERROR: couldn't allocate memory\n");
+			return NULL;
+		}
+		//Assume the string is correctly formatted
+		if(args = strtok(buf, "\t"))
+			e->id = atoi(args);
+		if(args = strtok(NULL, "\t"))
+			e->count = atoi(args);
+		if(args = strtok(NULL, "\t"))
+			e->from = args;
+		if(args = strtok(NULL, "\t"))
+			e->to = args;
+
+		dbg->edges[e->id] = e;
+
+		//Update correlated node
+		if (!add_out_edges(e->from, e)) {
+			fprintf(stdout, "ERROR: couldn't allocate memory\n");
+			return NULL;
+		}
+		if (!add_in_edges(e->to, e)) {
+			fprintf(stdout, "ERROR: couldn't allocate memory\n");
+			return NULL;
+		}
+
+		fgets(buf, 64, fp);
+	}
+
+	return dbg;
+}
---
+++ dbg/loader/loader.h
@@ -0,0 +1,33 @@
+#ifndef DBG_READER_H
+	#define DBG_READER_H
+
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <string.h>
+	#include <math.h>
+	#include "../data_structures.h"
+
+	/*
+	 * Alloc the external structure of the graph.
+	 * The two doubles are the dimension of nodes and edges
+	 * It also init all content to NULL
+	 *
+	 */
+	graph_t * alloc_graph(double, double);
+
+	/*
+	 * Loads nodes from the *.nodes file prompted
+	 * MUST be invoked after alloc_graph().
+	 *
+	 */
+	graph_t * load_nodes(graph_t *, char *);
+
+	/*
+	 * Load edges from the *.edges file prompted
+	 * It also loads edges into nodes.
+	 * MUST be invoked after load_nodes().
+	 *
+	 */
+	graph_t * load_edges(graph_t *, char *);
+
+#endif
