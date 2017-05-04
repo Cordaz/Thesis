f7c54bcd095137a370af8c047c8305af0f58d19a
Cordaz <andrea1.corneo@mail.polimi.it>
2017-05-04 10:27:34 +0200

    Added list implementation

---
+++ utilities/list.h
@@ -0,0 +1,15 @@
+#ifndef LIST_H
+	#define LIST_H
+
+	typedef struct list_s {
+		char * str;
+		struct list_s * next;
+	} list_t;
+
+	list_t * add(list_t *, char *);
+
+	int search(list_t *, char *);
+
+	void free(list_t *);
+
+#endif
